668c163a0f6a3b62ed2af0212aa380cbd8c1a87a
XinKeng <29268441+xinkeng0@users.noreply.github.com>
2024-01-20 20:47:39 +0800

    feat: My tailwind preflight.css

--- packages/core/tailwind.config.ts
+++ packages/core/tailwind.config.ts
@@ -1,4 +1,10 @@
 import type { Config } from "tailwindcss";
+// import plugin from "tailwindcss/plugin";
+// import fs from "fs";
+// import postcss from "postcss";
+// import path from "path";
+
+// const preflightSelector = ".papyrus-root";
 
 export default {
   darkMode: "class",
@@ -6,5 +12,25 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [
+    // https://github.com/tailwindlabs/tailwindcss/discussions/10332#discussioncomment-4698289
+    // plugin(function ({ addBase, addComponents, addUtilities }) {
+    //   const preflightStyles = postcss.parse(
+    //     fs.readFileSync(
+    //       path.resolve("./node_modules/tailwindcss/lib/css/preflight.css"),
+    //       "utf8",
+    //     ),
+    //   );
+    //   // Scope the selectors to specific components
+    //   preflightStyles.walkRules((rule) => {
+    //     rule.selectors = rule.selectors.map((s) => `papyrus-root ${s}`);
+    //     // rule.selector = rule.selectors.join(",\n");
+    //   });
+    //   // not working,because auto delete
+    //   addBase(preflightStyles.nodes);
+    // }),
+  ],
 } satisfies Config;
