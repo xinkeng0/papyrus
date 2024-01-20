3016b119b54a0ee962141431d4e2901b82977b12
XinKeng <29268441+xinkeng0@users.noreply.github.com>
2024-01-20 20:49:06 +0800

    feat: Add unplugin-icons/vite plugin

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Icons from "unplugin-icons/vite";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Icons({
+      compiler: "vue3",
+      autoInstall: true,
+    }),
     dts({
       insertTypesEntry: true,
     }),
