00c7494f7f49d570e2a7971bb067ed1bf92c9017
XinKeng <29268441+xinkeng0@users.noreply.github.com>
2024-01-24 20:50:15 +0800

    fix: Fix trailwind config content

--- packages/core/tailwind.config.ts
+++ packages/core/tailwind.config.ts
@@ -8,7 +8,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: "class",
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
