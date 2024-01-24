eb3a77b036f06392ecad39d207086f2085eb6076
XinKeng <29268441+xinkeng0@users.noreply.github.com>
2024-01-24 20:50:55 +0800

    feat: Use vuejsx and unplugin-icons auto import

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
+    Components({
+      resolvers: [IconsResolver()],
+    }),
     Icons({
       compiler: "vue3",
       autoInstall: true,
@@ -20,10 +29,12 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       // https://github.com/vitejs/vite/issues/1579
       // https://github.com/vitejs/vite/discussions/1736
+      //
       entry: {
         index: "src/index.ts",
         "@tiptap/vue3": "src/@tiptap-vue3.ts",
         "@tiptap/pm/model": "src/@tiptap-pm-model.ts",
+        "@tiptap/pm/state": "src/@tiptap-pm-state.ts",
       },
       name: "Papyrus",
       formats: ["es", "cjs"],
@@ -41,6 +52,7 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ["vue"],
       output: {
+        exports: "named",
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: "Vue",
