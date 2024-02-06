3824518ceccb2200deb08411016638d908e1433a
XinKeng <29268441+xinkeng0@users.noreply.github.com>
2024-02-06 21:43:53 +0800

    feat(example): Update example

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
+import { createPapyrus } from "@xinkeng0/papyrus";
+import { zhHans, en } from "@xinkeng0/papyrus/locale";
 import App from "./App.vue";
 
+const papyrus = createPapyrus({
+  locale: {
+    locale: "en",
+    fallback: "en",
+    messages: { zhHans, en },
+  },
+});
 const app = createApp(App);
-app.mount("#app");
+app.use(papyrus).mount("#app");
